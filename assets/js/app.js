b1294bb3bc93575578114f2546c0db6f59179cb3
coque <coquelet.samuel@gmail.com>
2021-12-23 15:17:18 +0100

    version 1.8 (coop)

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,98 @@ for (const input of inputs) {
     };
 }
 
+map.on('load', () => {
+    map.addSource('places', {
+// This GeoJSON contains features that include an "icon"
+// property. The value of the "icon" property corresponds
+// to an image in the Mapbox Streets style's sprite.
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'properties': {
+                        'description':
+                            '<strong>Théâtre Municipale Jean Ferrat</strong><p>Lien du ' +
+                            '<a href="https://theatre.fourmies.fr/" target="_blank">théâtre</a>' +
+                            ', Parvis François Mitterrand, Rue Saint-Louis, 59610 Fourmies</p>',
+                        'icon': 'theatre-15',
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [4.047129, 50.0155968]
+                    }
+                },
+                {
+                    'type': 'Feature',
+                    'properties': {
+                        'description':
+                            '<strong>Le Celtique</strong><p>Lieu ' +
+                            '<a href="https://madmens5finale.eventbrite.com/" target="_blank" title="Opens in a new window">' +
+                            'Celtique</a>, 12 Rue de Grenoble, 59610 Fourmies</p>',
+                        'icon': 'bar-15'
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [4.052752029370823, 50.01558677910946]
+                    }
+                },
+                {
+                    'type': 'Feature',
+                    'properties': {
+                        'description':
+                            '<strong>Chez Samuel</strong><p>Mon adresse personnelle que personne ne sait !' +
+                            ' <a href="https://tallulaeatbar.ticketleap.com/2012beachblanket/" target="_blank"' +
+                            ' title="Opens in a new window">Samuel</a> </p>',
+                        'icon': 'rocket-15'
+                    },
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [4.051103, 50.01865]
+                    }
+                },
+            ]
+        }
+    });
+// Add a layer showing the places.
+    map.addLayer({
+        'id': 'places',
+        'type': 'symbol',
+        'source': 'places',
+        'layout': {
+            'icon-image': '{icon}',
+            'icon-allow-overlap': true
+        }
+    });
+
+// When a click event occurs on a feature in the places layer, open a popup at the
+// location of the feature, with description HTML from its properties.
+    map.on('click', 'places', (e) => {
+// Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+
+// Ensure that if the map is zoomed out such that multiple
+// copies of the feature are visible, the popup appears
+// over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+
+        new mapboxgl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    });
+
+// Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'places', () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+// Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'places', () => {
+        map.getCanvas().style.cursor = '';
+    });
+});
